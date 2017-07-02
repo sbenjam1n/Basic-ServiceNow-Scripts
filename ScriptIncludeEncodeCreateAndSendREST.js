50011edb273398a223ab0705bfbbe0e55db8c554
Steven Benjamin <sbenja88@gmail.com>
2017-07-02 10:50:51 -0600

    Update ScriptIncludeEncodeCreateAndSendREST.js

--- ScriptIncludeEncodeCreateAndSendREST.js
+++ ScriptIncludeEncodeCreateAndSendREST.js
@@ -5,11 +5,8 @@ TeamsMessageCard.prototype = {
     initialize: function() {
     },
     
-	send: function (text, channel) {
+    send: function (text, channel) {
 
-    this.payload.text = text;
-    this.payload.channel = channel;
-        
     // Encode the payload as JSON
     var SNJSON = JSON; 
     var myjson = new SNJSON();
@@ -23,7 +20,7 @@ TeamsMessageCard.prototype = {
     return res;
     },
 	
-	endpoint: gs.getProperty('teams_message.default_endpoint'),
+    endpoint: gs.getProperty('teams_message.default_endpoint'), //https://outlook.office.com/webhook/3049fda5-ff69-493f-872f-03050ef41cb3@cfa792cf-7768-4341-8857-81754c2afa1f/IncomingWebhook/77818fdedd634cae9a81b0df97509a9f/5ac349f8-262c-485e-95e6-7dae56724cc3
     method: 'post',
     payload: {
               "themeColor": "0078D7",
