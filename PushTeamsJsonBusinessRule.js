45fb8a65f3fabbce4b914aac0305b8e9eea110e9
Steven Benjamin <sbenja88@gmail.com>
2017-07-02 10:59:25 -0600

    Update PushTeamsJsonBusinessRule.js

--- PushTeamsJsonBusinessRule.js
+++ PushTeamsJsonBusinessRule.js
@@ -7,7 +7,7 @@
 	teams.payload.sections.push({
         	"activityTitle": '[' + current.number.toString() +']('+'https://' + gs.getProperty('instance_name') + '.service-now.com/nav_to.do?uri=incident.do?sys_id=' + current.sys_id + ')',
         	"activitySubtitle": current.sys_created_on.toString(),
-        	"activityImage": gs.getProperty('owl_logo'),
+        	"activityImage": gs.getProperty('owl_logo'), //https://pbs.twimg.com/profile_images/628666536295731200/Pn9ghXNM.jpg
         	"activityText": current.short_description.toString()
         	 });
 	
